ec5b1dcf838e629d51eff946e8195032342dc6b0
conrellreddick <admin@dev.lawgen.org>
2018-08-07 16:35:03 -0400

    Added a MoviesController and a Random View

---
+++ Eivom/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using Eivom.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eivom.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            return View(movie);
+        }
+    }
+}
