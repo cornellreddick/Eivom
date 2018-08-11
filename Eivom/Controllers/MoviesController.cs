0097e40963b01fc1b282741827cf4f39a28901a1
conrellreddick <cornellreddick@outlook.com>
2018-08-11 19:02:31 -0400

    updated the movies controller.

--- Eivom/Controllers/MoviesController.cs
+++ Eivom/Controllers/MoviesController.cs
@@ -11,54 +11,20 @@ namespace Eivom.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
-        public ActionResult Customers()
+      public ViewResult Index()
         {
-            var movie = new Movie() { Name = "Shrek!" };
+            var movies = GetMovies();
 
-            var customers = new List<Customer>
-            {
-                new Customer {Name = "John Smith" }, 
-                new Customer {Name = "Mary Williams" }, 
-                
-
-            };
-
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-            
-            return View(viewModel);
-           
+            return View(movies);
         }
 
-        //public ActionResult Edit(int id)
-        //{
-        //    return Content("id=" + id);
-        //}
-
-        
-        //////movies
-        ////public ActionResult Index(int? pageIndex, string sortBy)
-        ////{
-        ////    if (!pageIndex.HasValue)
-        ////    {
-        ////        pageIndex = 1;
-        ////    }
-        ////    if (String.IsNullOrWhiteSpace(sortBy))
-        ////    {
-        ////        sortBy = "Name";
-        ////    }
-
-        ////    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        ////}
-        //[Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
-        //public ActionResult ByReleaseDate(int year, int month)
-        //{
-        //    return Content(year + "/" + month);
-        //}
-               
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Black Panther"},
+                new Movie {Id = 2, Name = "AntMan and Wasp"}
+            };
+        }
     }
 }
