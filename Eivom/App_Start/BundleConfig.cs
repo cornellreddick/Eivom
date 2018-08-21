97b1bab858b9f586ccc29441983d305872b48737
conrellreddick <cornellreddick@outlook.com>
2018-08-21 17:49:40 -0400

    Add a Birtdate property to the Customer class

--- Eivom/App_Start/BundleConfig.cs
+++ Eivom/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Eivom
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-darky.css",
+                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
