dc16954352d582b995eae27a917416858666dced
conrellreddick <cornellreddick@outlook.com>
2018-08-27 14:44:51 -0400

    Updated the database

--- Eivom/Migrations/201808252106485_AddMovie.cs
+++ Eivom/Migrations/201808252106485_AddMovie.cs
@@ -7,7 +7,7 @@ namespace Eivom.Migrations
     {
         public override void Up()
         {
-            AddColumn("dbo.Movies", "Genre", c => c.String());
+            
             AddColumn("dbo.Movies", "ReleaseDate", c => c.DateTime(nullable: false));
             AddColumn("dbo.Movies", "DateAdded", c => c.DateTime(nullable: false));
             AddColumn("dbo.Movies", "NumberInStock", c => c.Int(nullable: false));
@@ -18,7 +18,7 @@ namespace Eivom.Migrations
             DropColumn("dbo.Movies", "NumberInStock");
             DropColumn("dbo.Movies", "DateAdded");
             DropColumn("dbo.Movies", "ReleaseDate");
-            DropColumn("dbo.Movies", "Genre");
+           
         }
     }
 }
--- Eivom/Migrations/201808252134447_AddGenre.cs
+++ Eivom/Migrations/201808252134447_AddGenre.cs
@@ -7,11 +7,7 @@ namespace Eivom.Migrations
     {
         public override void Up()
         {
-            Sql("INSERT INTO Genres (Id, Name) VALUES (1, 'Action')");
-            Sql("INSERT INTO Genres (Id, Name) VALUES (2, 'Thriller')");
-            Sql("INSERT INTO Genres (Id, Name) VALUES (3, 'Family')");
-            Sql("INSERT INTO Genres (Id, Name) VALUES (4, 'Romance')");
-            Sql("INSERT INTO Genres (Id, Name) VALUES (5, 'Comedy')");
+          
         }
         
         public override void Down()
