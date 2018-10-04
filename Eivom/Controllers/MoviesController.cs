35fee19f151fdea22ecd8808f71ca724368929f0
conrellreddick <cornellreddick@outlook.com>
2018-10-04 18:11:17 -0400

    update the Movie Controller to allow imformation to be save

--- Eivom/Controllers/MoviesController.cs
+++ Eivom/Controllers/MoviesController.cs
@@ -58,6 +58,19 @@ namespace Eivom.Controllers
             return View("MovieForm", viewModel);
 
         }
+
+        public ActionResult Detail(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(movie);
+        }
+
         
         // GET: Movies/Random
         public ActionResult Random()
@@ -76,6 +89,26 @@ namespace Eivom.Controllers
             };
 
             return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
+
+            return RedirectToAction("Index", "Movies");
         }
     }
 }
