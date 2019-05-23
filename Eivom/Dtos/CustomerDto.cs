0450735b34e47c03d8af9e426add05868dca18bb
Cornell Reddick <cornellreddick@outlook.com>
2019-05-23 16:57:10 -0400

    Added a button to the Customer form view

--- Eivom/Dtos/CustomerDto.cs
+++ Eivom/Dtos/CustomerDto.cs
@@ -17,10 +17,10 @@ namespace Eivom.Dtos
 
         public bool IsSubscribedToNewsletter { get; set; }
 
-        //public MembershipType MembershipType { get; set; }
+        public MembershipType MembershipType { get; set; }
 
         //[Display(Name = "Membership Type")] 
-        public byte MembershipTypeId { get; set; }
+        //public byte MembershipTypeId { get; set; }
 
         //[Display(Name = "Date of Birth")]
         //[Min18YearsIfAMember]
